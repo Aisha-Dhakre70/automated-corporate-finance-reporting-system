efb83e4587b4b45eb9e9d437827c2c826bd8c625
Aisha Dhakre <147842490+Aisha-Dhakre70@users.noreply.github.com>
2026-04-19 23:23:30 +0530

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -2,11 +2,20 @@ CREATE DATABASE IF NOT EXISTS finance_db;
 
 USE finance_db;
 
-CREATE TABLE transactions (
+CREATE TABLE financial_data (
     id INT AUTO_INCREMENT PRIMARY KEY,
+
+    company VARCHAR(100),
     date DATE,
-    revenue FLOAT,
-    expense FLOAT,
+
+    revenue DOUBLE,
+    expense DOUBLE,
+    cogs DOUBLE,
+
+    gross_profit DOUBLE,
+    operating_profit DOUBLE,
+    net_profit DOUBLE,
+
     category VARCHAR(50),
     region VARCHAR(50)
 );
