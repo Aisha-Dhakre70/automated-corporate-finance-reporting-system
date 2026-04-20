9711b56d43166d7ae28eda8e5258ce2c7f4df9d5
Aisha Dhakre <147842490+Aisha-Dhakre70@users.noreply.github.com>
2026-04-20 19:43:25 +0530

    Update run_pipeline.bat

--- automation/run_pipeline.bat
+++ automation/run_pipeline.bat
@@ -1,5 +1,5 @@
 @echo off
-cd /d "C:\Users\User\OneDrive\Desktop\Semester - 8\automated-corporate-financial-reporting-system"
+cd /d "..\automated-corporate-financial-reporting-system"
 
 python -m pipeline.main_pipeline
 
